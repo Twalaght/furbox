2d56001112f82834368e8b24bcd3c9e694ac1bad
Twalaght <52785900+Twalaght@users.noreply.github.com>
2023-12-18 21:48:45 +1030

    E621 entrypoint stub and main runner
    - Placeholder config loader in main setup
    - Updated flake8 config to ignore unused imports in main

---
+++ furbox/main.py
@@ -0,0 +1,31 @@
+""" Main entrypoint to perform setup and transfer control to a runner's entry function. """
+
+import logging
+from pathlib import Path
+
+from furbox.models.config import Config
+from furbox.runners import cli
+from furbox.runners.e621 import download
+
+import yaml
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    """ TODO. """
+    args = cli.parse_args()
+
+    # TODO - Placeholder for config location
+    project_root = Path(__file__).parents[1]
+    config_path = project_root / "furbox_config.yaml"
+    with open(config_path) as f:
+        data = yaml.safe_load(f)
+
+    config = Config().parse_dict(data)
+
+    cli.run(args, config)
+
+
+if __name__ == "__main__":
+    main()
