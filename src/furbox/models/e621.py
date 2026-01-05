a9b13f1ee83b7beff2eb96bdf53216c1e6a46792
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-01-05 20:49:23 +1030

    Overhauled e621 models

--- src/furbox/models/e621.py
+++ src/furbox/models/e621.py
@@ -1,21 +1,8 @@
-""" Module implementing a model representation of e621 posts and pools, with parsing functionality.
-
-Example usage for a post: ::
-
-    # From API response
-    api_response = e621_connector.get_posts("vulpine")
-    api_posts = [Post().from_api(post) for post in api_response]
-
-    # From database dump
-    with open("posts.csv") as f:
-        database_posts = [
-            Post().from_database(row)
-            for row in csv.DictReader(f)
-        ]
-"""
+""" Model for e621 posts and pools, with parsing functionality. """
 import itertools
 from copy import deepcopy
 from datetime import datetime
+from enum import StrEnum
 from typing import Any, Self
 
 from fluffless.models.base_model import BaseModel
@@ -24,70 +11,50 @@ from fluffless.utils import logging
 logger = logging.getLogger(__name__)
 
 
-class E621Model(BaseModel):
-    """ Standardised parsing behaviour for E621 models. """
+def source_url_from_hash(md5_hash: str, extension: str) -> str:
+    """ Get a post source url from an MD5 hash.
 
-    @staticmethod
-    def char_to_bool(char: str) -> bool:
-        """ Convert a single character input to the corresponding boolean. """
-        return bool(char == "t")
+    Args:
+        md5_hash (str): MD5 hash of the post.
+        extension (str): File extension of the post.
 
-    @staticmethod
-    def parse_datetime(iso_datetime: str) -> datetime | None:
-        """ Parse varied formats of ISO datetime strings to a standardised datetime object.
-
-        Args:
-            iso_datetime (str): ISO time string.
-
-        Returns:
-            datetime | None: Converted datetime object from the ISO string, \
-                             or None if the input was not valid.
-        """
-        if not iso_datetime or len(iso_datetime) < 19:
-            return None
-
-        # Retain only the "%Y-%m-%d" and "%H:%M:%S" components of the ISO string
-        clipped_iso_datetime = iso_datetime[:10] + " " + iso_datetime[11:19]
-        return datetime.strptime(clipped_iso_datetime, "%Y-%m-%d %H:%M:%S")
-
-    @staticmethod
-    def source_url_from_hash(md5_hash: str, extension: str) -> str:
-        """ Get a post source url from an MD5 hash.
-
-        Args:
-            md5_hash (str): MD5 hash of the post.
-            extension (str): File extension of the post.
-
-        Returns:
-            str: Full quality source url corresponding to the input hash.
-        """
-        # As e621 is a superset of e926, e621 can be used as the base url for sources in all instances
-        base_url = "https://static1.e621.net/data"
-        return f"{base_url}/{md5_hash[:2]}/{md5_hash[2:4]}/{md5_hash}.{extension}"
+    Returns:
+        str: Full quality source url corresponding to the input hash.
+    """
+    # As e621 is a superset of e926, e621 can be used as the base url for sources in all instances
+    base_url = "https://static1.e621.net/data"
+    return f"{base_url}/{md5_hash[:2]}/{md5_hash[2:4]}/{md5_hash}.{extension}"
 
 
-class Post(E621Model):
+class Post(BaseModel):
     """ Dataclass representation of an e621 post. """
+
+    class Rating(StrEnum):
+        """ Permissible content rating associated with a post. """
+
+        SAFE = "s"
+        QUESTIONABLE = "q"
+        EXPLICIT = "e"
 
     class FileInfo(BaseModel):
         """ File information associated with a post. """
 
-        width:  int | None = None
-        height: int | None = None
-        size:   int | None = None
-        ext:    str | None = None
-        md5:    str | None = None
-        url:    str | None = None
+        width:  int
+        height: int
+        size:   int
+        ext:    str
+        md5:    str
+        url:    str
 
     class Flags(BaseModel):
         """ Post status flags. """
 
-        deleted:       bool | None = None
-        pending:       bool | None = None
-        flagged:       bool | None = None
-        rating_locked: bool | None = None
-        status_locked: bool | None = None
-        note_locked:   bool | None = None
+        deleted:       bool
+        pending:       bool
+        flagged:       bool
+        rating_locked: bool
+        status_locked: bool
+        note_locked:   bool
 
     class Relationships(BaseModel):
         """ Post relationship information. """
@@ -100,9 +67,9 @@ class Post(E621Model):
     class Score(BaseModel):
         """ Post score values. """
 
-        total: int | None = None
-        up:    int | None = None
-        down:  int | None = None
+        total: int
+        up:    int
+        down:  int
 
     class Tags(BaseModel):
         """ Post tags by category, and a combined full list. """
@@ -115,187 +82,147 @@ class Post(E621Model):
         invalid:    list[str] = []
         meta:       list[str] = []
         lore:       list[str] = []
-        all_tags:   list[str] = []
 
-    post_id:       int | None = None
-    uploader_id:   int | None = None
+        # Database tags are not split by normal categories, and as such are given their own category.
+        database:   list[str] = []
+
+        @property
+        def all_tags(self) -> list[str]:
+            """ Property combining all tags present in the post into one list. """
+            all_tags = [getattr(self, key) for key in list(type(self).model_fields.keys())]
+            return sorted(set(itertools.chain(*all_tags)))
+
+    post_id:       int
+    uploader_id:   int
     approver_id:   int | None = None
-    created_at:    datetime | None = None
+    created_at:    datetime
     updated_at:    datetime | None = None
-    rating:        str | None = None
-    description:   str | None = None
-    fav_count:     int | None = None
-    comment_count: int | None = None
-    change_seq:    int | None = None
+    rating:        Rating
+    description:   str
+    fav_count:     int
+    comment_count: int
+    change_seq:    int
     duration:      float | None = None
     is_favorited:  bool | None = None
     sources:       list[str] = []
     pools:         list[int] = []
-    file_info:     FileInfo | None = None
-    flags:         Flags | None = None
-    relationships: Relationships | None = None
-    score:         Score | None = None
-    tags:          Tags | None = None
+    file_info:     FileInfo
+    flags:         Flags
+    relationships: Relationships
+    score:         Score
+    tags:          Tags
 
     @classmethod
     def from_api(cls, api_response: dict[str, Any]) -> Self:
-        """ Create a post from an API response input.
-
-        Args:
-            api_response (dict[str, Any]): API JSON response data for a single post.
-
-        Returns:
-            Self: Reference to the dataclass itself.
-        """
+        """ Create a post model from an API response. """
         # Copy the API response such that the input data is not mangled during the remap
         data = deepcopy(api_response)
 
-        # Rename response fields to match their corresponding dataclass fields
+        # Rename response fields to match their corresponding model fields.
         data["tags"]["copyrights"] = data["tags"].pop("copyright")
         data["post_id"] = data.pop("id")
         data["file_info"] = data.pop("file")
 
-        # Combine all unique existing tags into the "all_tags" category
-        data["tags"]["all_tags"] = list(set(itertools.chain.from_iterable(data["tags"].values())))
-
-        # Convert ISO datetime strings to datetime objects
-        data["created_at"] = cls.parse_datetime(data["created_at"])
-        data["updated_at"] = cls.parse_datetime(data["updated_at"])
-
-        # Explicitly drop some API data which is not parsed to a post dataclass
-        data.pop("preview", None)
-        data.pop("sample", None)
-        data.pop("locked_tags", None)
-        data.pop("has_notes", None)
+        # Explicitly drop some API data which is not parsed to a post model.
+        for key in ("preview", "sample", "locked_tags", "has_notes"):
+            data.pop(key, None)
 
         return cls(**data)
 
-    def from_database(self, database_entry: dict[str, str]) -> Self:
-        """ Create a post from a database CSV row input.
+    @classmethod
+    def from_database(cls, database_entry: dict[str, Any]) -> Self:
+        """ Create a post from a database CSV row input, from e621's `db_export` endpoint. """
+        # Copy the database entry such that the input data is not mangled during the remap.
+        data = deepcopy(database_entry)
 
-        Args:
-            database_entry (dict[str, str]): Database CSV entry for a single post.
+        file_info = cls.FileInfo(
+            width=data["image_width"],
+            height=data["image_height"],
+            size=data["file_size"],
+            ext=data["file_ext"],
+            md5=data["md5"],
+            url=source_url_from_hash(data["md5"], data["file_ext"]),
+        )
 
-        Returns:
-            Self: Reference to the dataclass itself.
-        """
-        file_info = {
-            "width":  int(database_entry["image_width"]),
-            "height": int(database_entry["image_height"]),
-            "size":   int(database_entry["file_size"]),
-            "ext":    database_entry["file_ext"],
-            "md5":    database_entry["md5"],
-            "url":    self.source_url_from_hash(database_entry["md5"], database_entry["file_ext"]),
-        }
+        flags = cls.Flags(**{
+            name: data[f"is_{name}"] for name in
+            ("deleted", "pending", "flagged", "rating_locked", "status_locked", "note_locked")},
+        )
 
-        flags = {
-            "deleted":       self.char_to_bool(database_entry["is_deleted"]),
-            "pending":       self.char_to_bool(database_entry["is_pending"]),
-            "flagged":       self.char_to_bool(database_entry["is_flagged"]),
-            "rating_locked": self.char_to_bool(database_entry["is_rating_locked"]),
-            "status_locked": self.char_to_bool(database_entry["is_status_locked"]),
-            "note_locked":   self.char_to_bool(database_entry["is_note_locked"]),
-        }
-
-        relationships = {
-            "parent_id": int(parent_id) if (parent_id := database_entry["parent_id"]) else None,
-        }
-
-        score = {
-            "total": int(database_entry["score"]),
-            "up":    int(database_entry["up_score"]),
-            "down":  int(database_entry["down_score"]),
-        }
+        score = cls.Score(
+            total=data["score"],
+            up=data["up_score"],
+            down=data["down_score"],
+        )
 
         all_tags = []
         for tag_type in ["tag_string", "locked_tags"]:
             if tag_string := database_entry[tag_type]:
                 all_tags.extend(tag_string.split())
 
-        return self.parse_dict({
-            "post_id":       int(database_entry["id"]),
-            "uploader_id":   int(database_entry["uploader_id"]),
-            "approver_id":   int(approver_id) if (approver_id := database_entry.get("approver_id")) else None,
-            "created_at":    self.parse_datetime(database_entry["created_at"]),
-            "updated_at":    self.parse_datetime(database_entry["updated_at"]),
-            "rating":        database_entry["rating"],
-            "description":   database_entry["description"],
-            "fav_count":     int(database_entry["fav_count"]),
-            "comment_count": int(database_entry["comment_count"]),
-            "change_seq":    int(database_entry["change_seq"]),
-            "duration":      float(duration) if (duration := database_entry.get("duration")) else None,
-            "is_favorited":  None,
-            "sources":       database_entry["source"].splitlines(),
-            "pools":         None,
-            "file_info":     file_info,
-            "flags":         flags,
-            "relationships": relationships,
-            "score":         score,
-            "tags":          {"all_tags": all_tags},
-        })
+        return cls(
+            post_id=data["id"],
+            uploader_id=data["uploader_id"],
+            approver_id=data["approver_id"] or None,
+            created_at=data["created_at"],
+            updated_at=data["updated_at"] or None,
+            rating=data["rating"],
+            description=data["description"],
+            fav_count=data["fav_count"],
+            comment_count=data["comment_count"],
+            change_seq=data["change_seq"],
+            duration=data["duration"] or None,
+            sources=data["source"].splitlines(),
+            file_info=file_info,
+            flags=flags,
+            relationships=cls.Relationships(parent_id=data["parent_id"] or None),
+            score=score,
+            tags=cls.Tags(database=all_tags),
+        )
 
 
-class Pool(E621Model):
+class Pool(BaseModel):
     """ Dataclass representation of an e621 pool. """
 
-    pool_id:     int | None = None
-    name:        str | None = None
-    created_at:  datetime | None = None
-    updated_at:  datetime | None = None
-    description: str | None = None
-    active:      bool | None = None
-    category:    str | None = None
-    post_ids:    list[int] = []
-    post_count:  int | None = None
+    pool_id:     int
+    name:        str
+    created_at:  datetime
+    updated_at:  datetime
+    description: str
+    active:      bool
+    category:    str
+    post_ids:    list[int]
+    post_count:  int
 
     @classmethod
     def from_api(cls, api_response: dict[str, Any]) -> Self:
-        """ Create a pool from an API response input.
-
-        Args:
-            api_response (dict[str, Any]): API JSON response data for a single pool.
-
-        Returns:
-            Self: Reference to the dataclass itself.
-        """
-        # Copy the API response such that the input data is not mangled during the remap
+        """ Create a pool model from an API response. """
+        # Copy the API response such that the input data is not mangled during the remap.
         data = deepcopy(api_response)
 
-        # Rename response fields to match their corresponding dataclass fields
+        # Rename response fields to match their corresponding model fields.
         data["pool_id"] = data.pop("id")
         data["active"] = data.pop("is_active")
 
-        # Perform type conversions where required
-        data["created_at"] = cls.parse_datetime(data["created_at"])
-        data["updated_at"] = cls.parse_datetime(data["updated_at"])
-        data["active"] = cls.char_to_bool(data["active"])
-
-        # Explicitly drop some API data which is not parsed to a post dataclass
-        data.pop("creator_id", None)
-        data.pop("creator_name", None)
+        # Explicitly drop some API data which is not parsed to a pool model.
+        for key in ("creator_id", "creator_name"):
+            data.pop(key, None)
 
         return cls(**data)
 
-    def from_database(self, database_entry: dict[str, str]) -> Self:
-        """ Create a pool from a database CSV row input.
+    @classmethod
+    def from_database(cls, database_entry: dict[str, Any]) -> Self:
+        """ Create a pool from a database CSV row input, from e621's `db_export` endpoint. """
+        # Copy the database entry such that the input data is not mangled during the remap.
+        data = deepcopy(database_entry)
 
-        Args:
-            database_entry (dict[str, str]): Database CSV entry for a single pool.
+        # Extra post ID's from the string provided in the database, and get their length.
+        post_ids_str = data["post_ids"][1:-1]
+        data["post_ids"] = [int(post_id) for post_id in post_ids_str.split(",")] if post_ids_str else []
+        data["post_count"] = len(data["post_ids"])
 
-        Returns:
-            Self: Reference to the dataclass itself.
-        """
-        post_ids_str = database_entry["post_ids"][1:-1]
-        post_ids = [int(post_id) for post_id in post_ids_str.split(",")] if post_ids_str else []
+        # Rename response fields to match their corresponding model fields.
+        data["pool_id"] = data.pop("id")
+        data["active"] = data.pop("is_active")
 
-        return self.parse_dict({
-            "pool_id":     int(database_entry["id"]),
-            "name":        database_entry["name"],
-            "created_at":  self.parse_datetime(database_entry["created_at"]),
-            "updated_at":  self.parse_datetime(database_entry["updated_at"]),
-            "description": database_entry["description"],
-            "active":      self.char_to_bool(database_entry["is_active"]),
-            "category":    database_entry["category"],
-            "post_ids":    post_ids,
-            "post_count":  len(post_ids),
-        })
+        return cls(**data)  # type: ignore[invalid-argument-type]
