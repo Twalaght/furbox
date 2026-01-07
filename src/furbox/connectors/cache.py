f55029ced53ce546a9c01f04f84a98d71d608da9
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-01-07 20:08:31 +1030

    Cache improvements

--- src/furbox/connectors/cache.py
+++ src/furbox/connectors/cache.py
@@ -3,9 +3,8 @@
 Example usage of Cache: ::
 
     cache = Cache()
-    file_path = self.cache.resolve_path("posts_latest.csv")
-
-    if not self.cache.check(file_path):
+    file_path = cache.resolve_path("posts_latest.csv")
+    if not cache.check("posts_latest.csv"):
         download_file("db_export/posts.csv", file_path)
 
     with open(file_path, "r") as f:
@@ -22,11 +21,10 @@ logger = logging.getLogger(__name__)
 class Cache:
     """ Construct a cache, optionally in a specific directory and with a custom expiry length.
 
-    If no base directory is specified, `$XDG_CACHE_HOME` will be used if set, otherwise `$HOME/.cache`.
-    Values provided for `expiry_hours` and `expiry_minutes` are additive, and will not override each other.
+    If no base directory is specified, `$XDG_CACHE_HOME/furbox` will be used if set, otherwise `$HOME/.cache/furbox`.
 
     Args:
-        cache_dir (str | os.PathLike, optional): Custom directory to use as the base cache location. \
+        cache_dir (str | Path | None, optional): Custom directory to use as the base cache location. \
                                                  Defaults to None.
         expiry_hours (int, optional): Hours a cache file is valid for. Defaults to 24.
         expiry_minutes (int, optional): Minutes a cache file is valid for. Defaults to 0.
@@ -36,10 +34,12 @@ class Cache:
     """
 
     def __init__(self, cache_dir: str | Path | None = None, expiry_hours: int = 24, expiry_minutes: int = 0) -> None:
-        if not cache_dir and not (cache_dir := os.getenv("XDG_CACHE_HOME")):
-            cache_dir = Path.home() / ".cache"
+        if cache_dir:
+            self.cache_dir = Path(cache_dir)
+        else:
+            cache_dir = os.getenv("XDG_CACHE_HOME", Path.home() / ".cache")
+            self.cache_dir = Path(cache_dir) / "furbox"
 
-        self.cache_dir = Path(cache_dir) / "furbox"
         self.expiry_minutes = expiry_minutes + (expiry_hours * 60)
 
         if not self.cache_dir.exists():
@@ -51,12 +51,16 @@ class Cache:
 
     def check(self, file_path: str | Path) -> bool:
         """ Return True if the file path has a current valid cache entry, False otherwise. """
+        file_path = Path(file_path)
+        if not file_path.is_relative_to(self.cache_dir):
+            file_path = self.resolve_path(file_path)
+
         if not file_path.exists():
             return False
 
         cache_expiry_time = timedelta(minutes=self.expiry_minutes)
-        last_modified = datetime.fromtimestamp(file_path.lstat().st_mtime)
-        return not (datetime.now() - last_modified) > cache_expiry_time and self.expiry_minutes >= 0
+        last_modified = datetime.fromtimestamp(file_path.lstat().st_mtime, tz=None)
+        return not (datetime.now(tz=None) - last_modified) > cache_expiry_time and self.expiry_minutes >= 0
 
     def resolve_path(self, file_path: str | Path) -> Path:
         """ Resolve a file path relative to the cache directory. """
