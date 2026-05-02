b03cf9553f39974c8a137ea3bc102e3adf6aa547
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-05-02 22:15:03 +0930

    Updates to downloader

--- src/furbox/connectors/downloader.py
+++ src/furbox/connectors/downloader.py
@@ -3,6 +3,7 @@ import logging
 import shutil
 from multiprocessing import cpu_count, Pool
 from pathlib import Path
+from typing import NamedTuple, Self
 
 import requests
 
@@ -12,12 +13,63 @@ from furbox.utils.progress_bar import ProgressBar, ProgressBarStyle
 logger = logging.getLogger(__name__)
 
 
-def get_numbered_file_names(name: str, length: int, offset: int = 0, zero_pad: int | None = None) -> list[str]:
-    """ Generate files names numbered incrementally.
+class UrlFileTarget(NamedTuple):
+    """ Named tuple pair of a target download URL and the associated destination file name. """
+
+    url:                str
+    file_name:          str
+    download_directory: Path
+    extension:          str | None
+
+    @property
+    def output_path(self) -> Path:
+        """ Sanitise file name, extension, and download directory into a path. """
+        file_name_with_extension = self.file_name + (f".{self.extension}" if self.extension else "")
+        return self.download_directory / file_name_with_extension
+
+    @classmethod
+    def create(cls, url: str, file_name: str, download_directory: Path, extension: str | None = None) -> Self:
+        """ Parse a URL and file name into a clean URL and file path named tuple.
+
+        Args:
+            url (str): URL of file to download.
+            file_name (str): Name of file to download.
+            download_directory (Path): Directory to download file targets to.
+            extension (str | None, optional): \
+                Specify the extension to download the file as. Defaults to None, \
+                where extension is inferred by file preferentially, then URL.
+        """
+        # Strip query parameters from the input URL.
+        url = clean_url(url)
+
+        # Determine the extension to download to, sourced preferentially from:
+        # 1. The supplied extension argument.
+        # 2. Extension inferred from the file name.
+        # 3. Extension inferred from the stripped URL.
+        # If nothing is found, file is downloaded without any extension.
+        extensions = (
+            extension,
+            file_name.rsplit(".", maxsplit=1)[-1] if "." in file_name else None,
+            url.split(".")[-1] if "." in url else None,
+        )
+
+        return cls(
+            url=url,
+            file_name=file_name,
+            download_directory=Path(download_directory),
+            extension=next((ext for ext in extensions if ext), None),
+        )
+
+
+def get_numbered_file_names(
+    download_urls: list[str], download_directory: Path, name: str, offset: int = 0, zero_pad: int | None = None,
+) -> list[UrlFileTarget]:
+    """ Generate download file targets names numbered incrementally.
 
     Args:
+        download_urls (list[str]): List of URLs to generate file targets with numbered names for.
+        download_directory (Path): Directory to download file targets to.
         name (str): Base name for all files.
-        length (int): Number of file names to generate.
         offset (int, optional): Offset to all file numbers. Defaults to 0.
         zero_pad (int | None, optional): Use a fixed length zero padding for file names if provided. \
                                          Defaults to None.
@@ -25,10 +77,16 @@ def get_numbered_file_names(name: str, length: int, offset: int = 0, zero_pad: i
     Returns:
         list[str]: List of generated file names.
     """
-    # Calculate the standard zero padding, which will be at least 2 unless a custom pad is provided
-    page_len = len(str(offset + length + 1))
+    # Calculate the amount of zero padding to apply, which will be at least 2 unless a custom pad is provided.
+    num_urls = len(download_urls)
+    page_len = len(str(offset + num_urls + 1))
     zero_len = zero_pad or max(2, page_len)
-    return [f"{name} {str(num).zfill(zero_len)}" for num in range(offset + 1, offset + length + 1)]
+    file_names = [f"{name} {str(num).zfill(zero_len)}" for num in range(offset + 1, offset + num_urls + 1)]
+
+    return [
+        UrlFileTarget.create(url=url, file_name=file_name, download_directory=download_directory)
+        for url, file_name in zip(download_urls, file_names, strict=True)
+    ]
 
 
 def download_file(url: str, file_path: Path, description: str, leave_progress_bar: bool) -> None:
@@ -40,11 +98,16 @@ def download_file(url: str, file_path: Path, description: str, leave_progress_ba
         description (str): Description to use in progress bar.
         leave_progress_bar (bool): Leave the progress bar display after the download has finished.
     """
-    # Download the file as a stream, such that progress can be accurately displayed
+    # Download the file as a stream, such that progress can be accurately displayed.
     response = requests.get(url, stream=True, timeout=5)
     response.raise_for_status()
 
-    tmp_file_path = file_path.resolve().parent / f"_{file_path.name}"
+    # Create the parent directory if required.
+    parent_path = file_path.resolve().parent
+    parent_path.mkdir(parents=True, exist_ok=True)
+
+    # Download the file to a temporary file path.
+    tmp_file_path = parent_path / f"_{file_path.name}"
     with (
         Path(tmp_file_path).open("wb") as f,
         ProgressBar(
@@ -58,6 +121,7 @@ def download_file(url: str, file_path: Path, description: str, leave_progress_ba
             progress.advance(len(chunk))
             f.write(chunk)
 
+    # Once downloaded, move the temporarily file to the desired file path.
     shutil.move(
         src=tmp_file_path,
         dst=file_path,
@@ -77,38 +141,39 @@ def parallel_download(args: tuple[str, Path]) -> None:
             url (str): URL to download the file from.
             file_path (Path): File path to save the downloaded file to.
         """
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
+        # Create the parent directory if required.
+        parent_path = file_path.resolve().parent
+        parent_path.mkdir(parents=True, exist_ok=True)
 
+        # Download the file to a temporary file path.
         tmp_file_path = file_path.resolve().parent / f"_{file_path.name}"
         with Path(tmp_file_path).open("wb") as f:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
             f.write(response.content)
 
+        # Once downloaded, move the temporarily file to the desired file path.
         shutil.move(
             src=tmp_file_path,
             dst=file_path,
         )
 
+    # Call the inner function with supplied tuple of arguments.
     download(*args)
 
 
-def download_files(url_name_pairs: list[tuple[str, str]], download_dir: Path, description: str) -> None:
+# TODO
+def download_files(file_targets: list[UrlFileTarget], description: str) -> None:
     """ Download multiple files from a list of URL name pairs.
 
     Args:
-        url_name_pairs (list[tuple[str, str]]): List of tuples containing the URL to download from, \
-                                                and the file name to write to.
+        file_targets (list[UrlFileTarget]): List of download targets, pairing download URLs with file names.
         download_dir (Path): Directory to download all files to.
         description (str): Description to use in progress bar.
     """
-    # Strip HTTP query string params from the URL and format file names to include extension
-    download_args = []
-    for url, name in url_name_pairs:
-        cleaned_url = clean_url(url)
-        download_args.append((cleaned_url, Path(download_dir) / f"{name}.{cleaned_url.split('.')[-1]}"))
+    download_args = [(target.url, target.download_directory) for target in file_targets]
+    chunksize = len(file_targets) // cpu_count()
 
-    progress = ProgressBar(description, length=len(url_name_pairs))
-    for _ in Pool(cpu_count()).imap(parallel_download, download_args, chunksize=1):
-        progress.advance()
-
-    progress.close()
+    with ProgressBar(description, length=len(file_targets)) as progress:
+        for _ in Pool().imap(parallel_download, download_args, chunksize=chunksize):
+            progress.advance()
