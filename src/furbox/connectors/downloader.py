c132573ccc86ac5de9a8fa4afd27a4cc5251a863
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-05-09 22:51:25 +0930

    Updates to downloader

--- src/furbox/connectors/downloader.py
+++ src/furbox/connectors/downloader.py
@@ -1,10 +1,12 @@
 """ Module to download files and helper functions related to download operations. """
 import logging
 import shutil
-from multiprocessing import cpu_count, Pool
+import uuid
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from typing import NamedTuple, Self
 
+import backoff
 import requests
 
 from furbox.helpers.utils import clean_url
@@ -38,6 +40,9 @@ class UrlFileTarget(NamedTuple):
             extension (str | None, optional): \
                 Specify the extension to download the file as. Defaults to None, \
                 where extension is inferred by file preferentially, then URL.
+
+        Returns:
+            Self: Created UrlFileTarget instance.
         """
         # Strip query parameters from the input URL.
         url = clean_url(url)
@@ -75,7 +80,7 @@ def get_numbered_file_names(
                                          Defaults to None.
 
     Returns:
-        list[str]: List of generated file names.
+        list[UrlFileTarget]: List of generated download targets.
     """
     # Calculate the amount of zero padding to apply, which will be at least 2 unless a custom pad is provided.
     num_urls = len(download_urls)
@@ -134,44 +139,54 @@ def parallel_download(args: tuple[str, Path]) -> None:
     Args:
         args (tuple[str, Path]): Positional arguments to pass to `download()`.
     """
-    def download(url: str, file_path: Path) -> None:
+    @backoff.on_exception(backoff.expo, exception=requests.HTTPError, max_tries=3)
+    def download(url: str, file_path: Path, progress_bar: ProgressBar | None = None) -> None:
         """ Download a single file to disk.
 
         Args:
             url (str): URL to download the file from.
             file_path (Path): File path to save the downloaded file to.
+            progress_bar (ProgressBar | None): Progress bar to increment on successful download, if provided.
         """
         # Create the parent directory if required.
         parent_path = file_path.resolve().parent
         parent_path.mkdir(parents=True, exist_ok=True)
 
+        if file_path.exists() and file_path.is_file():
+            while True:
+                backup_path = parent_path / f"_backup_{uuid.uuid4()}{file_path.name}"
+                if not backup_path.exists():
+                    logger.warning(f"File '{file_path}' already exists, moving it to '{backup_path}'")
+                    file_path.rename(backup_path)
+
         # Download the file to a temporary file path.
         tmp_file_path = file_path.resolve().parent / f"_{file_path.name}"
         with Path(tmp_file_path).open("wb") as f:
-            response = requests.get(url, timeout=5)
+            response = requests.get(url, timeout=10)
             response.raise_for_status()
             f.write(response.content)
 
         # Once downloaded, move the temporarily file to the desired file path.
-        shutil.move(
-            src=tmp_file_path,
-            dst=file_path,
-        )
+        tmp_file_path.rename(file_path)
+
+        if progress_bar:
+            progress_bar.advance()
 
     # Call the inner function with supplied tuple of arguments.
     download(*args)
 
 
-def download_files(file_targets: list[UrlFileTarget], description: str) -> None:
+def download_files(file_targets: list[UrlFileTarget], description: str, threads: int = 8) -> None:
     """ Download a list of file targets.
 
     Args:
         file_targets (list[UrlFileTarget]): List of download targets, pairing download URLs with file names.
         description (str): Description to use in progress bar.
+        threads (int): Number of threads to use when downloading. Defaults to 8.
     """
-    download_args = [(target.url, target.download_directory) for target in file_targets]
-    chunksize = len(file_targets) // cpu_count()
-
-    with ProgressBar(description, length=len(file_targets)) as progress:
-        for _ in Pool().imap(parallel_download, download_args, chunksize=chunksize):
-            progress.advance()
+    with (
+        ProgressBar(description, length=len(file_targets)) as progress,
+        ThreadPoolExecutor(max_workers=threads) as executor,
+    ):
+        download_args = [(target.url, target.output_path, progress) for target in file_targets]
+        executor.map(parallel_download, download_args)
