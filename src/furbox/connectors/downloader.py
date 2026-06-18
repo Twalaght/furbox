242685aeb92c3083584446299db0be1c5234342f
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-06-18 23:05:36 +0930

    Overhauled download functionality for e621 (#16)

--- src/furbox/connectors/downloader.py
+++ src/furbox/connectors/downloader.py
@@ -1,9 +1,12 @@
 """ Module to download files and helper functions related to download operations. """
 import logging
 import shutil
-from multiprocessing import cpu_count, Pool
+import uuid
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+from typing import NamedTuple, Self
 
+import backoff
 import requests
 
 from furbox.helpers.utils import clean_url
@@ -12,23 +15,83 @@ from furbox.utils.progress_bar import ProgressBar, ProgressBarStyle
 logger = logging.getLogger(__name__)
 
 
-def get_numbered_file_names(name: str, length: int, offset: int = 0, zero_pad: int | None = None) -> list[str]:
-    """ Generate files names numbered incrementally.
+class UrlFileTarget(NamedTuple):
+    """ Named tuple pair of a target download URL and the associated destination file name. """
+
+    url:                str
+    file_name:          str
+    download_directory: Path
+    extension:          str | None
+
+    @property
+    def output_path(self) -> Path:
+        """ Sanitise file name, extension, and download directory into a path. """
+        file_name_with_extension = self.file_name + (f".{self.extension}" if self.extension else "")
+        return self.download_directory / file_name_with_extension
+
+    @classmethod
+    def create(cls, url: str, file_name: str, download_directory: Path, extension: str | None = None) -> Self:
+        """ Parse a URL and file name into a clean URL and file path named tuple.
+
+        Args:
+            url (str): URL of file to download.
+            file_name (str): Name of file to download.
+            download_directory (Path): Directory to download file targets to.
+            extension (str | None, optional): \
+                Specify the extension to download the file as. Defaults to None, \
+                where extension is inferred by file preferentially, then URL.
+
+        Returns:
+            Self: Created UrlFileTarget instance.
+        """
+        # Strip query parameters from the input URL.
+        url = clean_url(url)
+
+        # Determine the extension to download to, sourced preferentially from:
+        # 1. The supplied extension argument.
+        # 2. Extension inferred from the file name.
+        # 3. Extension inferred from the stripped URL.
+        # If nothing is found, file is downloaded without any extension.
+        extensions = (
+            extension,
+            file_name.rsplit(".", maxsplit=1)[-1] if "." in file_name else None,
+            url.split(".")[-1] if "." in url else None,
+        )
+
+        return cls(
+            url=url,
+            file_name=file_name,
+            download_directory=Path(download_directory),
+            extension=next((ext for ext in extensions if ext), None),
+        )
+
+
+def get_numbered_file_names(
+    download_urls: list[str], download_directory: Path, name: str, offset: int = 0, zero_pad: int | None = None,
+) -> list[UrlFileTarget]:
+    """ Generate download file targets names numbered incrementally.
 
     Args:
+        download_urls (list[str]): List of URLs to generate file targets with numbered names for.
+        download_directory (Path): Directory to download file targets to.
         name (str): Base name for all files.
-        length (int): Number of file names to generate.
         offset (int, optional): Offset to all file numbers. Defaults to 0.
         zero_pad (int | None, optional): Use a fixed length zero padding for file names if provided. \
                                          Defaults to None.
 
     Returns:
-        list[str]: List of generated file names.
+        list[UrlFileTarget]: List of generated download targets.
     """
-    # Calculate the standard zero padding, which will be at least 2 unless a custom pad is provided
-    page_len = len(str(offset + length + 1))
+    # Calculate the amount of zero padding to apply, which will be at least 2 unless a custom pad is provided.
+    num_urls = len(download_urls)
+    page_len = len(str(offset + num_urls + 1))
     zero_len = zero_pad or max(2, page_len)
-    return [f"{name} {str(num).zfill(zero_len)}" for num in range(offset + 1, offset + length + 1)]
+    file_names = [f"{name} {str(num).zfill(zero_len)}" for num in range(offset + 1, offset + num_urls + 1)]
+
+    return [
+        UrlFileTarget.create(url=url, file_name=file_name, download_directory=download_directory)
+        for url, file_name in zip(download_urls, file_names, strict=True)
+    ]
 
 
 def download_file(url: str, file_path: Path, description: str, leave_progress_bar: bool) -> None:
@@ -40,11 +103,16 @@ def download_file(url: str, file_path: Path, description: str, leave_progress_ba
         description (str): Description to use in progress bar.
         leave_progress_bar (bool): Leave the progress bar display after the download has finished.
     """
-    # Download the file as a stream, such that progress can be accurately displayed
+    # Download the file as a stream, such that progress can be accurately displayed.
     response = requests.get(url, stream=True, timeout=5)
     response.raise_for_status()
 
-    tmp_file_path = file_path.resolve().parent / f"_{file_path.name}"
+    # Create the parent directory if required.
+    parent_path = file_path.resolve().parent
+    parent_path.mkdir(parents=True, exist_ok=True)
+
+    # Download the file to a temporary file path.
+    tmp_file_path = parent_path / f"_{file_path.name}"
     with (
         Path(tmp_file_path).open("wb") as f,
         ProgressBar(
@@ -58,6 +126,7 @@ def download_file(url: str, file_path: Path, description: str, leave_progress_ba
             progress.advance(len(chunk))
             f.write(chunk)
 
+    # Once downloaded, move the temporarily file to the desired file path.
     shutil.move(
         src=tmp_file_path,
         dst=file_path,
@@ -70,45 +139,54 @@ def parallel_download(args: tuple[str, Path]) -> None:
     Args:
         args (tuple[str, Path]): Positional arguments to pass to `download()`.
     """
-    def download(url: str, file_path: Path) -> None:
+    @backoff.on_exception(backoff.expo, exception=requests.HTTPError, max_tries=3)
+    def download(url: str, file_path: Path, progress_bar: ProgressBar | None = None) -> None:
         """ Download a single file to disk.
 
         Args:
             url (str): URL to download the file from.
             file_path (Path): File path to save the downloaded file to.
+            progress_bar (ProgressBar | None): Progress bar to increment on successful download, if provided.
         """
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
+        # Create the parent directory if required.
+        parent_path = file_path.resolve().parent
+        parent_path.mkdir(parents=True, exist_ok=True)
 
+        if file_path.exists() and file_path.is_file():
+            while True:
+                backup_path = parent_path / f"_backup_{uuid.uuid4()}{file_path.name}"
+                if not backup_path.exists():
+                    logger.warning(f"File '{file_path}' already exists, moving it to '{backup_path}'")
+                    file_path.rename(backup_path)
+
+        # Download the file to a temporary file path.
         tmp_file_path = file_path.resolve().parent / f"_{file_path.name}"
         with Path(tmp_file_path).open("wb") as f:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
             f.write(response.content)
 
-        shutil.move(
-            src=tmp_file_path,
-            dst=file_path,
-        )
+        # Once downloaded, move the temporarily file to the desired file path.
+        tmp_file_path.rename(file_path)
 
+        if progress_bar:
+            progress_bar.advance()
+
+    # Call the inner function with supplied tuple of arguments.
     download(*args)
 
 
-def download_files(url_name_pairs: list[tuple[str, str]], download_dir: Path, description: str) -> None:
-    """ Download multiple files from a list of URL name pairs.
+def download_files(file_targets: list[UrlFileTarget], description: str, threads: int = 8) -> None:
+    """ Download a list of file targets.
 
     Args:
-        url_name_pairs (list[tuple[str, str]]): List of tuples containing the URL to download from, \
-                                                and the file name to write to.
-        download_dir (Path): Directory to download all files to.
+        file_targets (list[UrlFileTarget]): List of download targets, pairing download URLs with file names.
         description (str): Description to use in progress bar.
+        threads (int): Number of threads to use when downloading. Defaults to 8.
     """
-    # Strip HTTP query string params from the URL and format file names to include extension
-    download_args = []
-    for url, name in url_name_pairs:
-        cleaned_url = clean_url(url)
-        download_args.append((cleaned_url, Path(download_dir) / f"{name}.{cleaned_url.split('.')[-1]}"))
-
-    progress = ProgressBar(description, length=len(url_name_pairs))
-    for _ in Pool(cpu_count()).imap(parallel_download, download_args, chunksize=1):
-        progress.advance()
-
-    progress.close()
+    with (
+        ProgressBar(description, length=len(file_targets)) as progress,
+        ThreadPoolExecutor(max_workers=threads) as executor,
+    ):
+        download_args = [(target.url, target.output_path, progress) for target in file_targets]
+        executor.map(parallel_download, download_args)
--- src/furbox/runners/e621/download.py
+++ src/furbox/runners/e621/download.py
@@ -1,21 +1,91 @@
-""" TODO. """
+""" Download a search query or pool from e621. """
 import argparse
+import itertools
 import logging
+from collections import Counter
+from pathlib import Path
+from typing import cast
 
 from fluffless.utils import cli
+from rich.prompt import Confirm, Prompt
 
+from furbox.connectors.downloader import download_files, get_numbered_file_names, UrlFileTarget
+from furbox.connectors.e621 import E621Connector
 from furbox.models.config import Config
+from furbox.models.e621 import META_ARTISTS, Pool, Post
 from furbox.runners.e621 import _SUBPARSERS
 
 logger = logging.getLogger(__name__)
 
 
 PARSER = cli.add_parser("download", subparsers=_SUBPARSERS, help="TODO.")
-PARSER.add_argument("search_query", help="search query or pool number to download posts from")
+PARSER.add_argument("search_query", help="Search query to download posts from.")
+PARSER.add_argument(
+    "--pool",
+    action="store_true",
+    help="Download posts based on a pool, search query must be an integer if using pool mode.",
+)
 
 
 @cli.entrypoint(PARSER)
-def download(args: argparse.Namespace, config: Config) -> None:
-    """ TODO. """
-    del args, config
-    raise NotImplementedError
+def download(args: argparse.Namespace, config: Config) -> int | None:
+    """ Download from e621 based on search parameters. """
+    if config.e621 is None:
+        logger.error("Config requires `e621` to be defined to use the download utility")
+        return 1
+
+    search_query = cast(str, args.search_query)
+    pool_mode = args.pool
+
+    e621_connector = E621Connector(
+        username=config.e621.username,
+        api_key=config.e621.api_key,
+    )
+
+    # For a purely numeric search term, confirm if the user did not supply the pool flag.
+    if search_query.isnumeric() and not pool_mode:
+        pool_mode = Confirm("Search term is numeric but pool flag not provided, download as a pool?")
+
+    if pool_mode:
+        # Fetch pool information from the pools endpoint, and posts information using the general search.
+        pool = Pool.from_api(e621_connector.get_pool(search_query))
+        posts = [Post.from_api(post) for post in e621_connector.get_posts(f"pool:{search_query}")]
+        posts = sorted(posts, key=lambda x: pool.post_ids.index(x.post_id))
+
+        # Prompt the user for the title of the pool, defaulting to the name defined by the pool.
+        title = Prompt.ask("Pool title", default=pool.name.replace("_", " "))
+
+        # Set the artist of the pool by finding the most common artist tag applied to the posts.
+        artist_tags = list(itertools.chain(*[post.tags.artist for post in posts]))
+        artist_tags = [artist for artist in artist_tags if artist not in META_ARTISTS]
+        artist = Counter(artist_tags).most_common(1)[0][0].capitalize()
+        artist = Prompt.ask("Artist", default=artist)
+
+        # Generate download URLs and associated file name pairs.
+        file_targets = get_numbered_file_names(
+            download_urls=[post.file_info.url for post in posts],
+            download_directory=Path.cwd() / f"{artist} - {title}",
+            name=title,
+        )
+
+        download_files(file_targets=file_targets, description=f"Downloading {title}")
+    else:
+        download_dir = Prompt.ask(
+            prompt="Download directory",
+            default="".join([c if c.isalpha() else "_" for c in search_query]),
+        )
+
+        # Fetch pool information from the pools endpoint, and posts information using the general search.
+        posts = [Post.from_api(post) for post in e621_connector.get_posts(search_query)]
+
+        # Generate download URLs and associated file name pairs.
+        file_targets = [UrlFileTarget(
+            url=post.file_info.url,
+            file_name=str(post.post_id),
+            download_directory=Path.cwd() / download_dir,
+            extension=post.file_info.ext,
+        ) for post in posts]
+
+        download_files(file_targets=file_targets, description=f"Downloading '{search_query[:60]}'")
+
+    return None
