4ea627669fca87b2a5e93272cda9a02d63239c56
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-07-19 21:26:06 +0930

    Nix flake with shared

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "furbox, packaged using uv2nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    (import ./shared.nix {
+      packageName = "furbox";
+      pythonVersion = "python314";
+      src = ./.;
+    })
+    inputs;
+}
